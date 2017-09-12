d5d94dd5a4d2db556adda228189364f7e5c2ed55
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-09-12 15:03:41 +0200

    add email validation and saving

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,102 +1,131 @@
 
-    $(document).ready(function(){
+var isEmailValid = function(value) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(value && value != '' && value.length > 5 && re.test(value)) return true;
+    return false;
+}
+var sendEmail = function(email, callback){
+    var postCallUrl = 'http://network.us16.list-manage.com/subscribe/post-json?u=c183c6fd5297d1abedae2421f&id=ea106174e5&EMAIL=' + email;
+    $.post(postCallUrl, function( data ) {
+       console.log(data)
+       callback();
+      })
+}
+
+$(document).ready(function(){
+
+    var $emailContainer = $('.field-email-subscriprion');
+
+    //Header animation
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 100) {
+            $('.header').addClass('active');
+        }
+        else {
+            $('.header').removeClass('active');
+        }
+    });
+
+    //Email
+    $('#email-subcribe-button', $emailContainer).click(function() {
         
-                //Header animation
-                $(window).scroll(function() {
-                    if ($(this).scrollTop() >= 100) {
-                        $('.header').addClass('active');
-                    }
-                    else {
-                        $('.header').removeClass('active');
-                    }
-                });
-        
-                //Accordion
-                $('.faq h3').click(function() {
-                    $('.faq .item').removeClass('active');
-                    $(this).parent().toggleClass('active');
-                });
-        
-                $(".faq .row ul a").each(function(k,v){
-                    $(v).click(function(e){
-                        $(".faq .row ul a").removeClass('active');
-                        $(".faq .items .tab").removeClass('active');
-                        $(this).addClass('active');
-        
-                        var n = $(this).attr('data-id');
-                        $(".faq .items .tab-"+n).addClass('active');
-                        e.preventDefault();
-                    })
-                });
-        
-                //Scroll
-                $('.header .scroll').smoothScroll();
-        
-                //Menu
-                $('.menu-button').click(function() {
-                    $(this).toggleClass('active');
-                    $('.menu, html').toggleClass('active');
-                    return false;
-                });
-        
-                $('.menu .scroll').click(function() {
-                    $('.menu-button').toggleClass('active');
-                    $('.menu, html').removeClass('active');
-                    return false;
-                });
-        
-                //Count
-                $('#countdown').ClassyCountdown({
-                    end: $.now() + 110000,
-                    labelsOptions: {
-                        lang: {
-                            days: 'Days',
-                            hours: 'Hours',
-                            minutes: 'Minutes',
-                            seconds: 'Seconds'
-                        },
-                        style: ''
-                    },
-                    style: {
-                        element: '',
-                        labels: false,
-                        days: {
-                            gauge: {
-                                thickness: 0.2,
-                                bgColor: 'rgba(255, 255, 255, 0.3)',
-                                fgColor: 'rgba(255, 255, 255, 1)',
-                                lineCap: 'round'
-                            },
-                            textCSS: ''
-                        },
-                        hours: {
-                            gauge: {
-                                thickness: 0.2,
-                                bgColor: 'rgba(255, 255, 255, 0.3)',
-                                fgColor: 'rgba(255, 255, 255, 1)',
-                                lineCap: 'round'
-                            },
-                            textCSS: ''
-                        },
-                        minutes: {
-                            gauge: {
-                                thickness: 0.2,
-                                bgColor: 'rgba(255, 255, 255, 0.3)',
-                                fgColor: 'rgba(255, 255, 255, 1)',
-                                lineCap: 'round'
-                            },
-                            textCSS: ''
-                        },
-                        seconds: {
-                            gauge: {
-                                thickness: 0.2,
-                                bgColor: 'rgba(255, 255, 255, 0.3)',
-                                fgColor: 'rgba(255, 255, 255, 1)',
-                                lineCap: 'round'
-                            },
-                            textCSS: ''
-                        }
-                    }
-                });
-        
-            });
+        var emailValue = $('#email-input', $emailContainer).val();
+        if (isEmailValid(emailValue)) {
+            sendEmail(emailValue, function(){
+                $emailContainer.find('.done').removeClass('hidden');
+                $('#email-input', $emailContainer).val('')
+            })
+        }
+        return false;
+    });
+    
+
+    //Accordion
+    $('.faq h3').click(function() {
+        $('.faq .item').removeClass('active');
+        $(this).parent().toggleClass('active');
+    });
+
+    $(".faq .row ul a").each(function(k,v){
+        $(v).click(function(e){
+            $(".faq .row ul a").removeClass('active');
+            $(".faq .items .tab").removeClass('active');
+            $(this).addClass('active');
+
+            var n = $(this).attr('data-id');
+            $(".faq .items .tab-"+n).addClass('active');
+            e.preventDefault();
+        })
+    });
+
+    //Scroll
+    $('.header .scroll').smoothScroll();
+
+    //Menu
+    $('.menu-button').click(function() {
+        $(this).toggleClass('active');
+        $('.menu, html').toggleClass('active');
+        return false;
+    });
+
+    $('.menu .scroll').click(function() {
+        $('.menu-button').toggleClass('active');
+        $('.menu, html').removeClass('active');
+        return false;
+    });
+
+    //Count
+    $('#countdown').ClassyCountdown({
+        end: $.now() + 110000,
+        labelsOptions: {
+            lang: {
+                days: 'Days',
+                hours: 'Hours',
+                minutes: 'Minutes',
+                seconds: 'Seconds'
+            },
+            style: ''
+        },
+        style: {
+            element: '',
+            labels: false,
+            days: {
+                gauge: {
+                    thickness: 0.2,
+                    bgColor: 'rgba(255, 255, 255, 0.3)',
+                    fgColor: 'rgba(255, 255, 255, 1)',
+                    lineCap: 'round'
+                },
+                textCSS: ''
+            },
+            hours: {
+                gauge: {
+                    thickness: 0.2,
+                    bgColor: 'rgba(255, 255, 255, 0.3)',
+                    fgColor: 'rgba(255, 255, 255, 1)',
+                    lineCap: 'round'
+                },
+                textCSS: ''
+            },
+            minutes: {
+                gauge: {
+                    thickness: 0.2,
+                    bgColor: 'rgba(255, 255, 255, 0.3)',
+                    fgColor: 'rgba(255, 255, 255, 1)',
+                    lineCap: 'round'
+                },
+                textCSS: ''
+            },
+            seconds: {
+                gauge: {
+                    thickness: 0.2,
+                    bgColor: 'rgba(255, 255, 255, 0.3)',
+                    fgColor: 'rgba(255, 255, 255, 1)',
+                    lineCap: 'round'
+                },
+                textCSS: ''
+            }
+        }
+    });
+
+});
