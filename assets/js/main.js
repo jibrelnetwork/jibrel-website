f01ab9fe22b021cc41948433abd027e3e8c47586
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-09-14 12:21:40 +0200

    #9, working version of JSONP

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,12 @@ var isEmailValid = function(value) {
     return false;
 }
 
-function callbackName (data){}
 var sendEmail = function(email, callback){
-    var emailAddlUrl = 'https://network.us16.list-manage.com/subscribe/post?u=c183c6fd5297d1abedae2421f&id=ea106174e5&EMAIL=' + email;
+    var emailAddlUrl = '//network.us16.list-manage.com/subscribe/post-json?u=c183c6fd5297d1abedae2421f&id=ea106174e5&EMAIL=' + email;
     
-    $.post(emailAddlUrl, function(){
+    jsonp( emailAddlUrl, {param: "c"}, function (err, data) {
             callback();
     })
-
 }
 
 $(document).ready(function(){
@@ -139,3 +137,62 @@ $(document).ready(function(){
     });
 
 });
+/**
+ * uses 
+ * https://www.npmjs.com/package/jsonp 
+ * code ot perform correct email subscription
+ */
+function noop(){}
+function jsonp(url, opts, fn){
+    
+    var count = 0;
+
+    if ('function' == typeof opts) {
+      fn = opts;
+      opts = {};
+    }
+    if (!opts) opts = {};
+  
+    var prefix = opts.prefix || '__jp'; 
+    var id = opts.name || (prefix + (count++));
+  
+    var param = opts.param || 'callback';
+    var timeout = null != opts.timeout ? opts.timeout : 60000;
+    var enc = encodeURIComponent;
+    var target = document.getElementsByTagName('script')[0] || document.head;
+    var script;
+    var timer;
+  
+  
+    if (timeout) {
+      timer = setTimeout(function(){
+        cleanup();
+        if (fn) fn(new Error('Timeout'));
+      }, timeout);
+    }
+  
+    function cleanup(){
+      if (script.parentNode) script.parentNode.removeChild(script);
+      window[id] = noop;
+      if (timer) clearTimeout(timer);
+    }
+  
+    function cancel(){
+      if (window[id]) {
+        cleanup();
+      }
+    }
+  
+    window[id] = function(data){
+      cleanup();
+      if (fn) fn(null, data);
+    };
+
+    url += (~url.indexOf('?') ? '&' : '?') + param + '=' + enc(id);
+    url = url.replace('?&', '?');
+    script = document.createElement('script');
+    script.src = url;
+    target.parentNode.insertBefore(script, target);
+  
+    return cancel;
+  }
