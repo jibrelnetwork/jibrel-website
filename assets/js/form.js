62167c27be53e44eae195e264fb6333908802bfb
Ivan Violentov <violentoff@gmail.com>
2017-09-23 00:12:43 +0300

    Update form autocomplete fields

--- assets/js/form.js
+++ assets/js/form.js
@@ -24,8 +24,10 @@
     amount: validateNumber,
   }
 
+  var currencies = ['ETH', 'BTC', 'USD', 'EUR', 'CHF'];
+
   $(document).ready(function() {
-    $('.remodal-content input[type="checkbox"]').on('change', watchFormField);
+    $('.remodal-content input').on('change', watchFormField);
     $('.remodal-content input').on('input', watchFormField);
 
     $('#next-step').click(function(e) {
@@ -48,6 +50,8 @@
 
       e.stopPropagation();
     });
+
+    initAutocompletes();
   });
 
   function watchFormField(event) {
@@ -106,8 +110,8 @@
       isNextStepAllowed = (formFields.currency && formFields.amount);
 
       if (isNextStepAllowed) {
-        var amount = $('#form-amount').val();
-        var currency = $('#form-currency').val();
+        var amount = $('#amount').val();
+        var currency = $('#currency').val();
 
         $('#total-amount').html('' + amount + ' ' + currency);
       }
@@ -132,5 +136,88 @@
 
     $('#next-step').addClass('disabled');
     $('#steps-counter').html('' + currentStep + ' / ' + totalSteps);
+  }
+
+  function initAutocompletes() {
+    initAutocomplete('#country', countries);
+    initAutocomplete('#citizenship', countries);
+    initAutocomplete('#currency', currencies, true);
+  }
+
+  function initAutocomplete(selector, list, alwaysAll) {
+    var autocompleteRenderHandler = renderAutocomplete.bind(null, selector, list, alwaysAll);
+
+    autocompleteRenderHandler();
+
+    $(selector).on('input', autocompleteRenderHandler);
+
+    $(selector).click(function() {
+      hideAllAutocompletes(function() {
+        $(selector + '-list').addClass('active');
+        hideOnClickOutside(selector);
+      });
+    });
+
+    $(selector + '-container').click(function(e) {
+      e.stopPropagation();
+    });
+  }
+
+  function renderAutocomplete(selector, list, alwaysAll) {
+    var selectorList = selector + '-list';
+    var currentValue = $(selector).val().toLowerCase();
+
+    $(selectorList).removeClass('hidden');
+
+    var listItemsCount = 0;
+
+    var listItems = list.map(function (item) {
+      var isFound = (item.toLowerCase().indexOf(currentValue) > -1);
+      var isActive = (item.toLowerCase() === currentValue);
+
+      if (isFound || alwaysAll) {
+        listItemsCount += 1;
+
+        return '<li><a' + (isActive ? ' class="active" ' : '') + '>' + item + '</a></li>'
+      }
+
+      return '';
+    });
+
+    $(selectorList).html(listItems.join(''));
+
+    if (!listItemsCount) {
+      $(selectorList).addClass('hidden');
+    }
+
+    $(selectorList + ' a').click(function(e) {
+      $(selector).val($(this).text());
+      $(selector).change();
+      $(selectorList).removeClass('active');
+      renderAutocomplete(selector, list, alwaysAll);
+    });
+  }
+
+  function hideOnClickOutside(selector) {
+    function outsideClickListener() {
+      $(selector + '-list').removeClass('active');
+      removeClickListener();
+    }
+
+    function removeClickListener() {
+      document.removeEventListener('click', outsideClickListener);
+    }
+
+    setTimeout(function() {
+      document.addEventListener('click', outsideClickListener);
+    }, 50);
+  }
+
+  function hideAllAutocompletes(callback) {
+    $('.autocomplete').each(function() {
+      $(this).removeClass('active');
+
+      callback();
+    });
   }
 })(jQuery);
