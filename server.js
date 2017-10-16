99ca3ff2eff066b4eb0fb1b62c4c39a53f0254dd
Ivan Violentov <violentoff@gmail.com>
2017-10-16 21:08:40 +0300

    Add flags in header to switch languages

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
 
+app.get('/ko', function(req, res, next) {
+  res.redirect('/index-ko.html')
+})
+
 var port = process.env.PORT || 3000;
 app.listen(port);
 
