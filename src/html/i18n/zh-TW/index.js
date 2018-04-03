ff3aaf027be623cab9a47799f53ae6ea725c6215
Ivan Violentov <violentoff@gmail.com>
2018-04-03 23:43:36 +0300

    Adapt i18n for crowdin

---
+++ src/html/i18n/zh-TW/index.js
@@ -0,0 +1,33 @@
+const data = require('../data')
+
+const about = require('./about')
+const advisors = require('./advisors')
+const articles = require('./articles')
+const faq = require('./faq')
+const footer = require('./footer')
+const header = require('./header')
+const info = require('./info')
+const jnt = require('./jnt')
+const meta = require('./meta')
+const roadmap = require('./roadmap')
+const start = require('./start')
+const team = require('./team')
+const whitepaper = require('./whitepaper')
+
+module.exports = {
+  languageCode: 'zh-TW',
+  data: data,
+  about: about,
+  advisors: advisors,
+  articles: articles,
+  faq: faq,
+  footer: footer,
+  header: header,
+  info: info,
+  jnt: jnt,
+  meta: meta,
+  roadmap: roadmap,
+  start: start,
+  team: team,
+  whitepaper: whitepaper,
+}
