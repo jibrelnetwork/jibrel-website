374fcc0f463e3f8d664324db4632ac47e48d402f
Ivan Violentov <violentoff@gmail.com>
2018-04-28 14:22:40 +0300

    Move meta.json (fixes #84)

--- src/html/i18n/en-US/index.js
+++ src/html/i18n/en-US/index.js
@@ -1,10 +1,11 @@
+const meta = require('../meta')
+
 const about = require('./about').about
 const advisors = require('./advisors').advisors
 const articles = require('./articles').articles
 const faq = require('./faq').faq
 const footer = require('./footer').footer
 const header = require('./header').header
-const meta = require('./meta').meta
 const roadmap = require('./roadmap').roadmap
 const start = require('./start').start
 const team = require('./team').team
@@ -12,13 +13,13 @@ const whitepaper = require('./whitepaper').whitepaper
 
 module.exports = {
   languageCode: 'en-US',
+  meta: meta,
   about: about,
   advisors: advisors,
   articles: articles,
   faq: faq,
   footer: footer,
   header: header,
-  meta: meta,
   roadmap: roadmap,
   start: start,
   team: team,
